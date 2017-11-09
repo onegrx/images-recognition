8c572a65458d33e91ae63bb0b86cb86a99e8cf2c
onegrx <onegrx@gmail.com>
2017-11-09 10:43:34 +0100

    Draw vectors on plot

--- pca-kernel-trick/kernel_trick.py
+++ pca-kernel-trick/kernel_trick.py
@@ -22,22 +22,27 @@ def load_image(path):
 
     return points, colours
 
+def draw_vector(v0, v1, ax=None):
+    ax = ax or plt.gca()
+    arrowprops=dict(arrowstyle='->',
+                    linewidth=2,
+                    shrinkA=0, shrinkB=0)
+    ax.annotate('', v1, v0, arrowprops=arrowprops)
+
 
 
 def main():
-    points, colours = load_image('data/set2.bmp')
+    points, colours = load_image('data/set1.bmp')
 
+    X = np.array(points)
     pca = PCA(n_components=2)
-    pca.fit(points)
-    pca_points = pca.transform(points)
+    pca.fit(X)
 
-    x = [x for [x, y] in points]
-    y = [y for [x, y] in points]
-
-    # x = pca_points[:,0]
-    # y = pca_points[:,1]
-    plt.figure(figsize=(7,7))
-    plt.scatter(x, y, c=colours, marker=".")
+    plt.scatter(X[:, 0], X[:, 1], alpha=0.2, c=colours, marker=".")
+    for length, vector in zip(pca.explained_variance_, pca.components_):
+        v = vector * 3 * np.sqrt(length)
+        draw_vector(pca.mean_, pca.mean_ + v)
+    plt.axis('equal');
     plt.show()
 
 if __name__ == '__main__':
